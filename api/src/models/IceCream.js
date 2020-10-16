db0bbd33abdf01bb05a9d858c2bc55ef6b42976c
Krutakova E <krutakova@useit.pro>
2020-10-16 16:34:29 +0500

    Фильтры

--- api/src/models/IceCream.js
+++ api/src/models/IceCream.js
@@ -1,6 +1,6 @@
 const {Schema, model} = require('mongoose');
 
-const iceCream = new Schema({
+const IceCreamSchema = new Schema({
     title: {
       type: String,
       required: true
@@ -14,15 +14,48 @@ const iceCream = new Schema({
         required: true
     },
     price: {
-        amount: {
-            type: Number,
-            required: true
-        },
-        currency: {
-            code: String,
-            unit: String
-        }
+        amount:  Number,
+        currency: String
     }
 });
 
-module.exports = model('IceCream', iceCream);
+IceCreamSchema.methods.paginate = function (page, callback) {
+    const limit = 10;
+    const skip = page - 1;
+    let totalCount;
+
+    //count documents
+    this.count({}, function(err, count) {
+        if (err) {
+            totalCount = 0;
+        }
+        else{
+            totalCount = count;
+        }
+    })
+    if(totalCount == 0){
+        return callback('No Document in Database..', null);
+    }
+    //get paginated documents
+    this.find().skip(skip).limit(limit).exec(function(err, docs){
+
+        if(err){
+            return callback('Error Occured', null);
+        }
+        else if(!docs){
+            return callback('Docs Not Found', null);
+        }
+        else{
+            const result = {
+                "totalRecords" : totalCount,
+                "page": page,
+                "nextPage": page + 1,
+                "result": docs
+            };
+            return callback(null, result);
+        }
+
+    });
+}
+
+module.exports = model('IceCream', IceCreamSchema);
